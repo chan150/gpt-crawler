58301f974b6b0d697a1f58a6beaee290bccf1649
Himchan Park <chan150@kaist.ac.kr>
2023-11-30 17:51:35 +0900

    OpenAI 에서 jsonl 파일 읽을 수 없는 문제

--- config.ts
+++ config.ts
@@ -3,6 +3,6 @@ import { Config } from "./src/config";
 export const defaultConfig: Config = {
   url: "https://www.builder.io/c/docs/developers",
   match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
+  maxPagesToCrawl: 3,
   outputFileName: "output.json",
 };
